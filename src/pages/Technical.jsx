56ae79bf1ab294621a7a538728e7f04dcdedefa7
kunalambarte17 <ambartekunal17@gmail.com>
2025-01-31 22:24:15 +0530

    Technical

---
+++ src/pages/Technical.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar/Navbar"
+
+const Technical = () => {
+  return (
+    <Navbar/>
+  )
+}
+
+export default Technical
--- src/pages/Tehnical.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Tehnical = () => {
-  return (
-    <div>Tehnical hello i am kunal</div>
-  )
-}
-
-export default Tehnical
