8f49295cac186c6f2637e39871476e4a15d9d70c
Laukik Lanje <laukiklanje24@gmail.com>
2025-02-02 17:43:00 +0530

    ready to go

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,37 +11,37 @@ const Footer = () => {
           <span className="font-extrabold text-2xl">YASH Team </span>
         </p>
         <p className="text-xl mt-4">
-          <a href="">
+          <a href="https://github.com/laukik-24/YASH-25.0">
             <i className="fa-brands fa-github"></i>
           </a>
           &nbsp; <span>View source code of this project</span>
         </p>
       </div>
-      <div className="footer-right">
+      <div className="footer-right m-0 ">
         <div className="social-icons">
           <a
-            href="https://www.linkedin.com/in/laukik-lanje/"
+            href="https://www.ycce.edu/"
             target="_blank"
             className="cursor-pointer"
           >
             <i className="fa-brands fa-linkedin foot-icon"></i>
           </a>
           <a
-            href="https://www.instagram.com/laukik2404/"
+            href="https://www.instagram.com/yash_25.0_?igsh=MTZoYXZhaDZ2NnhseA=="
             target="_blank"
             className="cursor-pointer"
           >
             <i className="fa-brands fa-instagram foot-icon"></i>
           </a>
           <a
-            href="mailto:laukiklanje24@gmail.com"
+            href="mailto:abc@gmail.com"
             target="_blank"
             className="cursor-pointer"
           >
             <i className="fa-regular fa-envelope foot-icon"></i>
           </a>
         </div>
-        <p className="text-lg flex text-white mt-4">
+        <p className="text-lg flex text-white mt-4 mb-8">
           Made by &nbsp;{" "}
           <a
             href="https://portfolio-laukik.vercel.app/"
